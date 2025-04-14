087a2df12c334c94f5646813117ad15215df1138
Misha Iakovlev <misha@fuzzylabs.ai>
2025-04-14 12:05:34 +0100

    Revert prompt template

--- sre_agent/client.py
+++ sre_agent/client.py
@@ -24,6 +24,16 @@ if CHANNEL_ID is None:
     logger.error("Environment variable CHANNEL_ID is not set.")
     raise ValueError("Environment variable CHANNEL_ID is not set.")
 
+
+# PROMPT = f"""I have an error with my application, can you check the logs for the
+# cart service, I only want you to check the pods logs, look up only the 100 most
+# recent logs. Feel free to scroll up until you find relevant errors that contain
+# reference to a file, once you have these errors and the file name, get the file
+# contents of the path src for the repository microservices-demo in the organisation
+# fuzzylabs. Keep listing the directories until you find the file name and then get the
+# contents of the file. Once you have diagnosed the error please report this to the
+# following slack channel: {CHANNEL_ID}."""
+
 # Break long string into multiple lines for better readability
 PROMPT = (
     f"Can you list pull requests for the microservices-demo repository in the "
