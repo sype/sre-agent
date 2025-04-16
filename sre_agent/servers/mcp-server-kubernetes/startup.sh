bd0c75584a5b4b822aa5a56b241da1bb871112d8
Chris <32800386+Christopher-Norman@users.noreply.github.com>
2025-04-16 15:53:36 +0100

    [FA-49] Create agent cluster (#19)
    * [FA-49] feature: added deployment manifests for orchestrator and mcp-servers
    
    * [FA-49] chore: placeholder naming
    
    * Add Kubernetes authentication and context update on pod start-up for Kubernetes MCP server
    
    * Add more variables to K8s manifests
    
    * [FA-49] refactor: renamed agent service to orchestrator
    
    * [FA-49] chore: comment deletion
    
    * Add Kubernetes deployment documentation first-pass
    
    * Remove envsubst variable from script
    
    * Migrate env variables to secrets
    
    * Update README sample output
    
    * Add dev bearer token to sample output
    
    * Add flexiblity to the Kubernetes MCP server start-up script
    
    * Add env variables for Kubernetes MCP start-up script to local compose files
    
    * Add error handling to the Kubernetes MCP server start-up script
    
    * Add information about finding external IP
    
    * Minor README fixes
    
    * [FA-49] feature: single bash script for applying k8s manifests
    
    * [FA-49] fix: slack env variables
    
    * [FA-49] refactor: routing same port
    
    * Rephrase warning
    
    * Add removed line from README from merge conflict
    
    * README edits from review comments
    
    * Remove Kube config mount from Docker compose files
    
    * Add more information about node size
    
    ---------
    
    Co-authored-by: YazIbrahim <29870124+YazIbrahim@users.noreply.github.com>

---
+++ sre_agent/servers/mcp-server-kubernetes/startup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -euo pipefail
+
+function log_error_and_exit {
+  echo "❌ Failed to update kubeconfig:"
+  echo "$1"
+  exit 1
+}
+
+echo "🔧 Updating kubeconfig for EKS cluster..."
+if ! output=$(aws eks update-kubeconfig --region $AWS_REGION --name $TARGET_EKS_CLUSTER_NAME 2>&1); then
+  log_error_and_exit "$output"
+fi
+
+echo "✅ Kubeconfig updated successfully."
+echo "🚀 Starting Node.js application..."
+exec node dist/index.js
